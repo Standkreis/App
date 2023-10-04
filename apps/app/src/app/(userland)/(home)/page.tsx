0fc4ab664d18e676b7d087bbce6be0be96a8ed43
Sven Reiser <svreiser@gmail.com>
2023-10-04 20:25:11 +0200

    adds some better version of home for brand testing

--- apps/app/src/app/(userland)/(home)/page.tsx
+++ apps/app/src/app/(userland)/(home)/page.tsx
@@ -1,31 +1,43 @@
+import Container from '@/components/Container'
+import Page from '@/components/Page'
 import ToggleMode from '@/components/ToggleMode'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="bg-default">
-      <h1 className="text-primary text-3xl font-bold">Standkreis</h1>
-      <p className="text-secondary">
-        This is going to be the standkreis website
-      </p>
+    <Page>
+      <Container>
+        <div className="container mx-auto ">
+          <h1>Standkreis</h1>
+          <h2>Standkreis</h2>
+          <h3>Standkreis</h3>
+          <h4>Standkreis</h4>
+          <h5>Standkreis</h5>
+          <h6>Standkreis</h6>
 
-      <ToggleMode />
+          <p className="font-weight-100">
+            Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+            Konsonantien leben die Blindtexte.
+          </p>
 
-      <div className="bg-paper mx-auto flex max-w-sm items-center space-x-4 rounded-xl p-6 shadow-lg ">
-        <div className="shrink-0">
-          <Image
-            className="h-12 w-12"
-            src="/img/logo.svg"
-            alt="ChitChat Logo"
-            width={60}
-            height={60}
-          />
+          <blockquote>
+            Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile
+            in den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion
+            werden die Blindtexte beherrscht – ein geradezu unorthographisches
+            Leben.
+          </blockquote>
+
+          <ToggleMode />
+
+          <div className="bg-paper mx-auto flex max-w-sm items-center space-x-4 rounded-xl p-6 shadow-lg ">
+            <div>
+              <h5>ChitChat</h5>
+              <p className="!text-hint font-weight-100">
+                You have a new message!
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <div className="text-primary text-xl font-medium">ChitChat</div>
-          <p className="text-hint">You have a new message!</p>
-        </div>
-      </div>
-    </div>
+      </Container>
+    </Page>
   )
 }
