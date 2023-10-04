15ed3ade9fba0efc64487d03955dd221bd2021e1
Sven Reiser <svreiser@gmail.com>
2023-10-04 17:12:14 +0200

    Update page.tsx

--- apps/app/src/app/(userland)/(home)/page.tsx
+++ apps/app/src/app/(userland)/(home)/page.tsx
@@ -1,27 +1,8 @@
-'use client'
-
-import React from 'react'
-
 export default function Home() {
-  let mode = localStorage.getItem('theme')
-  React.useEffect(() => {
-    mode = localStorage.getItem('theme')
-  }, [])
-  console.log(mode)
-
-  const toggleMode = () => {
-    if (mode === 'dark') {
-      console.log('mode', mode, 'should toggle to light')
-      localStorage.theme = 'light'
-      document.documentElement.classList.remove('dark')
-    }
-
-    if (mode === 'light') {
-      console.log('mode', mode, 'should toggle to dark')
-      localStorage.theme = 'dark'
-      document.documentElement.classList.add('dark')
-    }
-  }
-
-  return <button onClick={toggleMode}>Toggle Mode</button>
+  return (
+    <div>
+      <h1>Standkreis</h1>
+      <p>This is going to be the standkreis website</p>
+    </div>
+  )
 }
