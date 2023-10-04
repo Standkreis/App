5450f58e6dd527f8ef3f81df65eaf06939d1e9f2
Sven Reiser <svreiser@gmail.com>
2023-10-04 22:14:40 +0200

    adds brandcolors

--- apps/app/tailwind.config.ts
+++ apps/app/tailwind.config.ts
@@ -11,8 +11,30 @@ const config: Config = {
       display: ['__Berkshire_Swash_721f85'],
       body: ['__Vollkorn_8ff8c3'],
     },
-
-    extend: {},
+    extend: {
+      colors: {
+        'gray': {
+          50: '#F5F5F5',
+          100: '#E6EDED',
+          200: '#C6D8DA',
+          300: '#A6C5CA',
+          400: '#73AAB2',
+          500: '#4D8C95',
+          600: '#2B646C',
+          700: '#1F4247',
+          800: '#0D2225',
+          900: '#0A1A1D',
+          950: '#081416',
+        },
+        'primary': {
+          darker: '#008F76',
+          dark: '#00B292',
+          main: '#00CDA8',
+          light: '#00EAC0',
+          lighter: '#00FFD1',
+        }
+      }
+    },
   },
   variants: {},
   plugins: [],
