eecc8e3410c4e133c822293ef10ea13c1761ee26
Sven Reiser <svreiser@gmail.com>
2023-10-04 22:54:01 +0200

    first attemt for PWA

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -7,8 +7,43 @@ const vollkorn = Vollkorn({ subsets: ['latin'], weight: ['400'] })
 const berkshire = Berkshire_Swash({ subsets: ['latin'], weight: ['400'] })
 
 export const metadata: Metadata = {
-  title: 'Standkreis',
-  description: 'Standkreis App',
+  applicationName: 'TITLE',
+  title: 'TITLE',
+  description: 'DESCRIPTION',
+  formatDetection: {
+    telephone: false,
+  },
+  viewport: {
+    minimumScale: 1,
+    initialScale: 1,
+    width: 'device-width',
+    userScalable: false,
+    viewportFit: 'cover',
+  },
+  themeColor: '',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'TITLE',
+    startupImage: {
+      url: 'images/apple_splash.png',
+    },
+  },
+  twitter: {
+    card: 'summary',
+    title: 'TITLE',
+    description: 'DESCRIPTION',
+    creator: 'CREATOR',
+    images: 'IMAGE_URL',
+  },
+  openGraph: {
+    type: 'website',
+    title: 'TITLE',
+    description: 'DESCRIPTION',
+    siteName: 'TITLE',
+    url: 'DOMAIN',
+    images: 'IMAGE_URL',
+  },
 }
 
 type Props = {
