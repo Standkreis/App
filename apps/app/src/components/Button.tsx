fb0929b644afc65ec278d96b4692c576be9d9a2e
Sven Reiser <svreiser@gmail.com>
2023-10-04 18:53:09 +0200

    adds basic button component

--- apps/app/src/components/Button.tsx
+++ apps/app/src/components/Button.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { ReactNode } from 'react'
 
-const Button = () => {
-  return <div>Button</div>
+type Props = {
+  children: ReactNode
+  onClick: () => void
+}
+
+const Button = (props: Props) => {
+  const { children, onClick } = props
+
+  return (
+    <button
+      onClick={onClick}
+      className="rounded-xl bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
