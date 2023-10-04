98fc0560aa25e8579f4baa52808bcecb0195f8d2
Sven Reiser <svreiser@gmail.com>
2023-10-04 23:09:43 +0200

    adds more assets and meta variables

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -6,10 +6,22 @@ import Providers from './Providers'
 const vollkorn = Vollkorn({ subsets: ['latin'], weight: ['400'] })
 const berkshire = Berkshire_Swash({ subsets: ['latin'], weight: ['400'] })
 
+const META = {
+  title: 'Standkreis',
+  description:
+    'Reshape our ecological, social and economic systems towards a more sustainable future.',
+  creator: 'Standkreis',
+  url: 'standkreis.com',
+  images: {
+    twitter: 'images/twitter.png',
+    appleSplash: 'images/apple_splash.png',
+  },
+}
+
 export const metadata: Metadata = {
-  applicationName: 'TITLE',
-  title: 'TITLE',
-  description: 'DESCRIPTION',
+  applicationName: META.title,
+  title: META.title,
+  description: META.description,
   formatDetection: {
     telephone: false,
   },
@@ -20,29 +32,29 @@ export const metadata: Metadata = {
     userScalable: false,
     viewportFit: 'cover',
   },
-  themeColor: '',
+  themeColor: '#00CDA8',
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
-    title: 'TITLE',
+    title: META.title,
     startupImage: {
       url: 'images/apple_splash.png',
     },
   },
   twitter: {
     card: 'summary',
-    title: 'TITLE',
-    description: 'DESCRIPTION',
-    creator: 'CREATOR',
-    images: 'IMAGE_URL',
+    title: META.title,
+    description: META.description,
+    creator: META.creator,
+    images: META.images.twitter,
   },
   openGraph: {
     type: 'website',
-    title: 'TITLE',
-    description: 'DESCRIPTION',
-    siteName: 'TITLE',
-    url: 'DOMAIN',
-    images: 'IMAGE_URL',
+    title: META.title,
+    description: META.description,
+    siteName: META.title,
+    url: META.url,
+    images: META.images.twitter,
   },
 }
 
