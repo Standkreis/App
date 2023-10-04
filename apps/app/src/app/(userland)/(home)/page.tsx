076f1a0f944986b30ff9d12b8a3ff6c55d575062
Sven Reiser <svreiser@gmail.com>
2023-10-05 00:57:31 +0200

    adds Styleguide and replaces home content

--- apps/app/src/app/(userland)/(home)/page.tsx
+++ apps/app/src/app/(userland)/(home)/page.tsx
@@ -6,59 +6,13 @@ export default function Home() {
   return (
     <Page>
       <Container className="max-w-4xl">
-        <h1>Standkreis</h1>
+        <h1 className="mb-4">Home</h1>
         <p>
-          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
-          Konsonantien leben die Blindtexte.
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem.
         </p>
-
-        <h2>Standkreis</h2>
-        <p>
-          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
-          Konsonantien leben die Blindtexte.
-        </p>
-
-        <h3>Standkreis</h3>
-        <p>
-          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
-          Konsonantien leben die Blindtexte.
-        </p>
-
-        <h4>Standkreis</h4>
-        <p>
-          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
-          Konsonantien leben die Blindtexte.
-        </p>
-
-        <h5>Standkreis</h5>
-        <p>
-          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
-          Konsonantien leben die Blindtexte.
-        </p>
-
-        <h6>Standkreis</h6>
-        <p>
-          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
-          Konsonantien leben die Blindtexte.
-        </p>
-
-        <blockquote>
-          Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in
-          den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion
-          werden die Blindtexte beherrscht – ein geradezu unorthographisches
-          Leben.
-        </blockquote>
-
-        <ToggleMode />
-
-        <div className="bg-paper mx-auto flex max-w-sm items-center space-x-4 rounded-xl p-6 shadow-lg ">
-          <div>
-            <h5>ChitChat</h5>
-            <p className="!text-hint font-weight-100">
-              You have a new message!
-            </p>
-          </div>
-        </div>
       </Container>
     </Page>
   )
---
+++ apps/app/src/app/(userland)/styleguide/page.tsx
@@ -0,0 +1,63 @@
+import Container from '@/components/Container'
+import Page from '@/components/Page'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Page>
+      <Container className="max-w-4xl">
+        <h1>Standkreis</h1>
+        <p>
+          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+          Konsonantien leben die Blindtexte.
+        </p>
+
+        <h2>Standkreis</h2>
+        <p>
+          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+          Konsonantien leben die Blindtexte.
+        </p>
+
+        <h3>Standkreis</h3>
+        <p>
+          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+          Konsonantien leben die Blindtexte.
+        </p>
+
+        <h4>Standkreis</h4>
+        <p>
+          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+          Konsonantien leben die Blindtexte.
+        </p>
+
+        <h5>Standkreis</h5>
+        <p>
+          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+          Konsonantien leben die Blindtexte.
+        </p>
+
+        <h6>Standkreis</h6>
+        <p>
+          Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und
+          Konsonantien leben die Blindtexte.
+        </p>
+
+        <blockquote>
+          Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in
+          den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion
+          werden die Blindtexte beherrscht – ein geradezu unorthographisches
+          Leben.
+        </blockquote>
+
+        <div className="bg-paper mx-auto flex max-w-sm items-center space-x-4 rounded-xl p-6 shadow-lg ">
+          <div>
+            <h5>ChitChat</h5>
+            <p className="text-hint">You have a new message!</p>
+          </div>
+        </div>
+      </Container>
+    </Page>
+  )
+}
+
+export default page
