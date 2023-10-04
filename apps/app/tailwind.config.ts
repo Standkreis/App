68260099e830f8e07df869b815ded0e19c9366f2
Sven Reiser <svreiser@gmail.com>
2023-10-04 18:53:14 +0200

    Update tailwind.config.ts

--- apps/app/tailwind.config.ts
+++ apps/app/tailwind.config.ts
@@ -1,20 +1,18 @@
 import type { Config } from 'tailwindcss'
+import { ThemeConfig } from 'tailwindcss/types/config'
 
 const config: Config = {
+  darkMode: 'class',
+  purge: ['./src/**/*.tsx'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    typography: (theme: ThemeConfig) => ({}),
+    extend: {},
   },
-  plugins: [],
+  variants: {},
+  plugins: [require('@tailwindcss/typography')],
 }
 export default config
