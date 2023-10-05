99c46736f2254d8e5237a9d3f9c6ff7996a795ab
Sven Reiser <svreiser@gmail.com>
2023-10-05 15:38:57 +0200

    adds other pages to test navigtion

--- apps/app/src/app/(userland)/shop/page.tsx
+++ apps/app/src/app/(userland)/shop/page.tsx
@@ -1,11 +1,11 @@
 import Container from '@/components/Container'
 import Page from '@/components/Page'
 
-export default function Home() {
+export default function Shop() {
   return (
     <Page>
       <Container className="max-w-4xl">
-        <h1 className="mb-4">Home</h1>
+        <h1 className="mb-4">Shop</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
           commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
