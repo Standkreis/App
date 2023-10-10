65400c4af597ecb92ebdcd5774e07c7ba2fe9b6b
Sven Reiser <svreiser@gmail.com>
2023-10-10 17:20:22 +0200

    adds basic MDX support to next

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
-
 const withPWA = require('next-pwa')({
   dest: 'public',
 })
+const withMDX = require('@next/mdx')()
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   trailingSlash: true,
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = withPWA(withMDX(nextConfig))
