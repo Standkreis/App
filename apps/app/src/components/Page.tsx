aff5b6aae50261e4c0068751ae60300c67877339
Sven Reiser <svreiser@gmail.com>
2023-10-14 20:27:39 +0200

    Update Page.tsx

--- apps/app/src/components/Page.tsx
+++ apps/app/src/components/Page.tsx
@@ -9,7 +9,13 @@ const Page = (props: Props) => {
   const { children, className } = props
 
   return (
-    <div className={` bg-default min-h-screen px-4 xl:px-0 ${className}`}>
+    <div
+      style={{
+        backgroundImage:
+          'radial-gradient(50% 50% at 50% 50%, #185D67 0%, #1B3134 100%)',
+      }}
+      className={`min-h-screen px-4 xl:px-0 ${className}`}
+    >
       {children}
     </div>
   )
